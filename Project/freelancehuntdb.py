05fd083017dbcb0ab7cb74e19aa473b1a3196aa7
Alex Aleynikov <106587233+AlexAleynikov@users.noreply.github.com>
2023-04-26 14:06:02 +0300

    Add files via upload

--- Project/freelancehuntdb.py
+++ Project/freelancehuntdb.py
@@ -1,36 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import sqlite3
+from utils import DB_NAME, HEADERS, connect_db, create_table, insert_data
 
-DB_NAME = 'projects.db'
+
 HOST = 'https://freelancehunt.com/ua'
 URL = 'https://freelancehunt.com/ua/projects'
-HEADERS = {
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
-}
-
-
-def connect_db():
-    conn = sqlite3.connect(DB_NAME)
-    return conn
-
-
-def create_table(conn):
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS projects
-                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title TEXT NOT NULL,
-                    link TEXT NOT NULL,
-                    description TEXT)''')
-    conn.commit()
-
-
-def insert_data(conn, data):
-    cur = conn.cursor()
-    cur.execute('''INSERT INTO projects(title, link, description)
-                   VALUES (?, ?, ?)''', (data['title'], data['link'], data['description']))
-    conn.commit()
 
 
 def get_html(url, params=''):
